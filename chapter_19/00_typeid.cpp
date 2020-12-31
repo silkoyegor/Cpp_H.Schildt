0585aad0bf6fbb86e1336c417610f2fd0ae7317b
Yegor Silko <silko.yegor@gmail.com>
2020-12-31 19:42:24 +0200

    type_info

---
+++ chapter_19/00_typeid.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <typeinfo> // typeid
+/* Оператор typeid возвращает ссылку на объект типа type_info
+// В классе type_info определены следующие public-члены:
+// bool operator=(const type_info &ob);
+// bool operator!=(const type_info &ob);
+// bool before(const type_info &ob);
+// const char *name();
+*/
+
+using namespace std;
+
+
+
+class myclass {};
+class Base {
+  virtual void func() {};
+};
+
+class Derived1: public Base {};
+class Derived2: public Base {};
+
+
+int main()
+{
+  int i, j;
+  float f;
+  myclass obj;
+
+  cout << "Тип переменной i: " << typeid(i).name() << endl;
+  cout << "Тип переменной f: " << typeid(f).name() << endl;
+  cout << "Тип переменной obj: " << typeid(obj).name() << endl;
+ 
+  if(typeid(i) == typeid(j)) cout << "Типы переменных i и j одинаковы.\n";
+  if(typeid(i) != typeid(f)) cout << "Типы переменных i и f неодинаковы.\n";
+  
+
+  cout << "\n\n";
+  Base *p, baseob;
+  Derived1 ob1;
+  Derived2 ob2;
+  p = &baseob;
+  cout << "Переменная p указывает на объект типа " << typeid(*p).name() << endl;
+  p = &ob1;
+  cout << "Переменная p указывает на объект типа " << typeid(*p).name() << endl;
+  p = &ob2;
+  cout << "Переменная p указывает на объект типа " << typeid(*p).name() << endl;
+  
+
+  return 0;
+}
